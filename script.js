cf1e5ef79488e6fb7172805fbe977302a0d31e0e
Margherita <mangoguglia@gmail.com>
2020-07-29 18:22:05 +0200

    ajax-ex-boolflix

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente. Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// 1. Titolo
-// 2. Titolo Originale
-// 3. Lingua
-// 4. Voto
-
 //chiamata: https://api.themoviedb.org/3/search/movie
 //api_key: e99307154c6dfb0b4750f6603256716d
 
 function addSearchClickListener(){
 var target = $('#search');
-target.click(getMovies);
+target.click(startSearch);
+}
+
+function startSearch(query){
+
+  var target = $('#query');
+  var query = target.val();
+  target.val('');
+
+  var targetResults = $('#results');
+  targetResults.text('');
+
+  getMovies(query);
+  getSeries(query);
 }
 
 function getMovies(){
-  var target = $('#query');
-  var query = target.val();
-  var apiKey = 'e99307154c6dfb0b4750f6603256716d';
-  target.val('');
 
 $.ajax({
 
@@ -24,7 +27,7 @@ $.ajax({
   method: 'GET',
   data: {
 
-    'api_key': apiKey,
+    'api_key': 'e99307154c6dfb0b4750f6603256716d',
     'query': query,
     'language': 'it'
   },
@@ -34,25 +37,27 @@ $.ajax({
 
     var movies = data['results'];
 
-    //voto stelle numerico
-
-    var results = movies.map(function(voto) {return voto.vote_average;});
-    console.log('result',results);
-
-    for (var i = 0; i < results.length; i++) {
-      var stars = results[i];
-      var star = ((Math.ceil(stars))/2).toFixed(0);
-      console.log('star', star);
-   }
-
     // scaffold di Handelbars
-    var target = $('#results')
+    var target = $('#results ul');
     var template = $('#movie-template').html();
     var compiled = Handlebars.compile(template);
-    target.text('');
+
 
     for (var i = 0; i < movies.length; i++) {
       var movie = movies[i];
+
+      var vote = movie['vote_average'];
+      movie.stars = getStars(vote);
+
+      var lang = movie['original_language'];
+      movie.flag = getFlag(lang);
+
+      var title = movie['title'];
+
+      movie.title = '<h3 class="movie-title"></h3>' + title;
+
+      var originalTitle = movie['original_title'];
+      movie.original_title = '<p class="original-title"></p>' + originalTitle;
 
       var movieHTML = compiled(movie);
       target.append(movieHTML);
@@ -64,17 +69,39 @@ $.ajax({
 });
 }
 
-
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+function getSeries(){
 
 
+}
 
 
+function getStars(vote){
+  vote = Math.ceil(vote / 2);
 
+  voteHTML = '';
+
+  for (var j = 0; j < 5; j++) {
+    if ( j < vote){
+      voteHTML += '<i class="fas fa-star"></i>';
+    } else{
+      voteHTML += '<i class="far fa-star"></i>';
+    }
+  }
+  return voteHTML;
+}
+
+function getFlag(lang){
+
+  if(lang ==='it' || lang === 'en' || lang === 'ja'){
+    return `<img class="flag" src="img/${lang}.png">`;
+  }
+  return lang;
+}
 
 function init(){
   addSearchClickListener();
+
+  // debug;
+  startSearch();
 }
 $(document).ready(init);
